76a0b41b115d1aad85450006c8a1f4437486902f
FafnirNithhoggr <felixmonteiroo.o@gmail.com>
2024-02-09 15:47:29 +0000

    added aclad left and birdge

---
+++ Assets/Scripts/AcladLogicLeft.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcladLogicLeft : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.CompareTag("Aclad")) {
+            
+            Transform otherTransform = other.gameObject.GetComponent<Transform>();
+            otherTransform.Rotate(0, -90, 0);
+
+            AcladLogic acladLogic = other.gameObject.GetComponent<AcladLogic>();
+            acladLogic.SetDirection(otherTransform.forward);
+        }
+    }
+}
