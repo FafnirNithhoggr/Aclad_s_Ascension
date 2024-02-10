8147bb76d3bb11b4ea6a06bc28e8bf4d9d545af6
FafnirNithhoggr <72742666+FafnirNithhoggr@users.noreply.github.com>
2024-02-10 17:45:12 +0000

    added fan and abstracted aclad right and left to aclad side

--- Assets/Scripts/AcladLogicSide.cs
+++ Assets/Scripts/AcladLogicSide.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AcladLogicRight : MonoBehaviour
+public class AcladLogicSide : MonoBehaviour
 {
+
+    public bool isLeft = false;
+
     private void OnTriggerEnter(Collider other) {
 
         if (other.gameObject.CompareTag("Aclad")) {
             
             Transform otherTransform = other.gameObject.GetComponent<Transform>();
-            otherTransform.Rotate(0, 90, 0);
-
+            if (isLeft){
+                otherTransform.Rotate(0, -90, 0);
+            }else{
+                otherTransform.Rotate(0, 90, 0);
+            }
             AcladLogic acladLogic = other.gameObject.GetComponent<AcladLogic>();
             acladLogic.SetDirection(otherTransform.forward);
         }
--- Assets/Scripts/Fan.cs
+++ Assets/Scripts/Fan.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AcladLogicUp : MonoBehaviour
+public class Fan : MonoBehaviour
 {
-    public float upForce = 5f;
+
+    public float windForce = 5f;
+
 
     private void OnTriggerEnter(Collider other) {
+
         if (other.gameObject.CompareTag("Aclad")) {
             Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
-            rb.AddForce(Vector3.up * upForce, ForceMode.Impulse);
+            rb.AddForce(transform.forward * windForce, ForceMode.Impulse);
         }
     }
 }
