d98d325511c4b85555b311682a3d2b28c7450286
Johnny-droid <joaoricardoralves@gmail.com>
2024-02-12 13:22:37 +0000

    Added more controls to the camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    public float rotationSpeed = 5.0f;
+    public Vector3 focusPoint;
+
+    public float lowerBound;
+
+    public float upperBound;
+    public float rotationSpeed;
 
     void Update()
     {
@@ -18,5 +23,24 @@ public class CameraController : MonoBehaviour
         {
             transform.RotateAround(Vector3.zero, Vector3.up, -1 * rotationSpeed * Time.deltaTime);
         }
+
+        // Move the camera up and down with the W and S keys
+        if (Input.GetKey(KeyCode.W) && focusPoint.y < upperBound)
+        {
+            transform.Translate(0, 0.1f, 0);
+            focusPoint.y += 0.1f;
+        }
+
+        // Don't move the camera focus point below the ground
+        if (Input.GetKey(KeyCode.S) && focusPoint.y > lowerBound)
+        {
+            transform.Translate(0, -0.1f, 0);
+            focusPoint.y -= 0.1f;
+        }
+
+        // Zoom in and out with the mouse wheel
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        transform.Translate(0, 0, scroll * 10);
+        
     }
 }
