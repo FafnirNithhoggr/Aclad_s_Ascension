bd44bdf77d8103e3031ca217cd8f3897d81ee94c
Johnny-droid <joaoricardoralves@gmail.com>
2024-02-14 15:57:59 +0000

    Added Very Simple Menu (still missing level selector)

---
+++ Assets/MenuLogic.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLogic : MonoBehaviour
+{
+    public void ClickPlay()
+    {
+        // Load the game scene
+        SceneManager.LoadScene("DevelopmentLevel");
+    }
+}
