59f920a2d7a8a14877a546fdb9190dbca749e6bf
FafnirNithhoggr <72742666+FafnirNithhoggr@users.noreply.github.com>
2024-02-28 02:54:50 +0000

    aclads suspension. added raycasting for suspension, they float! still have a lot of issues but its a start

--- Assets/Scripts/AcladLogic.cs
+++ Assets/Scripts/AcladLogic.cs
@@ -9,6 +9,21 @@ public class AcladLogic : MonoBehaviour
 
     private Vector3 direction;
     // Start is called before the first frame update
+
+
+    public float maxSuspensionLength;
+    public float suspensionMultiplier;
+    public float dampSenstivity;
+    public float maxDamp;
+
+    private float oldDist;
+
+
+    private void Awake()
+    {
+        oldDist = maxSuspensionLength;
+    }
+
     void Start()
     {
         direction = transform.forward;
@@ -18,6 +33,30 @@ public class AcladLogic : MonoBehaviour
     void Update()
     {
         transform.position += direction * speed * Time.deltaTime;
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity)){
+            if(hit.collider != null)
+            {
+                
+
+                Vector3 suspensionForce = Mathf.Clamp(maxSuspensionLength - hit.distance, 0, maxSuspensionLength) * suspensionMultiplier * transform.up;
+
+                suspensionForce += transform.up * Mathf.Clamp((oldDist - hit.distance) * dampSenstivity, 0, maxDamp);
+
+                rb.AddForce(suspensionForce, ForceMode.Force);
+
+                Debug.DrawRay(transform.position, Vector3.down * maxSuspensionLength, Color.red);
+                Debug.DrawRay(transform.position, suspensionForce, Color.green);
+                
+                //rb.AddForceAtPosition((Mathf.Clamp(maxSuspensionLength - hit.distance, 0, maxSuspensionLength) * suspensionMultiplier * transform.up + transform.up * Mathf.Clamp((oldDist- hit.distance) * dampSenstivity, 0, maxDamp)) * Time.deltaTime, transform.position);
+            }
+            else
+            {
+
+            }
+            oldDist = hit.distance;
+        }
     }
 
     public void SetDirection(Vector3 newDirection) {
