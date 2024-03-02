223cfb1d45ac7d650466fcdc5e31d20366f709f4
Johnny-droid <joaoricardoralves@gmail.com>
2024-03-02 16:36:34 +0000

    Laser now destroys!!!!!!!!!!!!!!!1

--- Assets/Scripts/AcladLogicLaser.cs
+++ Assets/Scripts/AcladLogicLaser.cs
@@ -7,41 +7,61 @@ public class AcladLogicLaser : MonoBehaviour
     [SerializeField] private LineRenderer lineRenderer;
     [SerializeField] private float laserSpeed;
     [SerializeField] private float maxLength;
+    [SerializeField] private Transform laserOrigin;
+    [SerializeField] private GameObject explosionPrefab;
     private GameObject target;
     private Vector3 hitPoint;
     private float distanceToTarget;
     private float currentDistance;
+    private bool reachedTarget = false;
 
     // Start is called before the first frame update
     void Start()
     {
         // Send a raycast to the target
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, maxLength)) {
+        if (Physics.Raycast(laserOrigin.position, laserOrigin.forward, out hit, maxLength)) {
             target = hit.collider.gameObject;
             hitPoint = hit.point;
             distanceToTarget = hit.distance;
         } else {
             target = null;
-            hitPoint = transform.position + transform.forward * maxLength;
+            hitPoint = laserOrigin.position + laserOrigin.forward * maxLength;
             distanceToTarget = maxLength;
         }
     }
 
     // Update is called once per frame
     private void FixedUpdate() {
-        // Move the laser to the target
-        if (lineRenderer != null && lineRenderer.enabled) {
-            currentDistance += laserSpeed * Time.fixedDeltaTime;
-            if (currentDistance >= distanceToTarget) {
-                //Deactivate();
-                if (target != null) {
-                    Destroy(target);
-                }
-            } else {
-                lineRenderer.SetPosition(0, transform.position);
-                lineRenderer.SetPosition(1, transform.position + transform.forward * currentDistance);
-            }
+        // If the line renderer is not enabled, return
+        if (lineRenderer == null || !lineRenderer.enabled) {
+            return;
         }
+
+        // If the target is reached, move the laser forward
+        if (reachedTarget) {
+            currentDistance -= laserSpeed * Time.fixedDeltaTime;
+            if (currentDistance <= 0) {
+                lineRenderer.enabled = false;
+                return;
+            }
+            lineRenderer.SetPosition(0, hitPoint - laserOrigin.forward * currentDistance);
+            lineRenderer.SetPosition(1, hitPoint);
+            return;
+        }
+        
+        // If the target is not reached, move the laser to the target
+        currentDistance += laserSpeed * Time.fixedDeltaTime;
+        if (currentDistance >= distanceToTarget) {
+            if (target != null) {
+                reachedTarget = true;
+                Instantiate(explosionPrefab, hitPoint + target.transform.up * 0.1f, Quaternion.identity);
+                Destroy(target);
+            }
+        } else {
+            lineRenderer.SetPosition(0, laserOrigin.position);
+            lineRenderer.SetPosition(1, laserOrigin.position + laserOrigin.forward * currentDistance);
+        }
+        
     }
 }
