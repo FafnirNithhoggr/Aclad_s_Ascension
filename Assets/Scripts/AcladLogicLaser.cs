f3bbd325d9840c18e78b26523e36f49463f90701
Johnny-droid <joaoricardoralves@gmail.com>
2024-03-06 17:30:09 +0000

    Solved laser problem

--- Assets/Scripts/AcladLogicLaser.cs
+++ Assets/Scripts/AcladLogicLaser.cs
@@ -12,23 +12,30 @@ public class AcladLogicLaser : MonoBehaviour
     private GameObject target;
     private Vector3 hitPoint;
     private float distanceToTarget;
-    private float currentDistance;
+    private float currentDistance = 0.0f;
     private bool reachedTarget = false;
 
     // Start is called before the first frame update
     void Start()
     {
         // Send a raycast to the target
-        RaycastHit hit;
-        if (Physics.Raycast(laserOrigin.position, laserOrigin.forward, out hit, maxLength)) {
-            target = hit.collider.gameObject;
-            hitPoint = hit.point;
-            distanceToTarget = hit.distance;
-        } else {
-            target = null;
-            hitPoint = laserOrigin.position + laserOrigin.forward * maxLength;
-            distanceToTarget = maxLength;
+        RaycastHit[] hits = Physics.RaycastAll(laserOrigin.position, laserOrigin.forward, maxLength);
+        foreach (RaycastHit h in hits) {
+            // Get the grandparent of the collider
+            if (h.collider.transform.parent != null && h.collider.transform.parent.parent != null) {
+                if (h.collider.transform.parent.parent.gameObject.tag == "Terrain") {
+                    target = h.collider.transform.parent.parent.gameObject;
+                    hitPoint = h.point;
+                    distanceToTarget = h.distance;
+                    return;
+                }
+            }
         }
+
+        // If no target is found, set the hit point to the end of the laser
+        target = null;
+        hitPoint = laserOrigin.position + laserOrigin.forward * maxLength;
+        distanceToTarget = maxLength;
     }
 
     // Update is called once per frame
@@ -53,8 +60,8 @@ public class AcladLogicLaser : MonoBehaviour
         // If the target is not reached, move the laser to the target
         currentDistance += laserSpeed * Time.fixedDeltaTime;
         if (currentDistance >= distanceToTarget) {
+            reachedTarget = true;
             if (target != null) {
-                reachedTarget = true;
                 Instantiate(explosionPrefab, hitPoint + target.transform.up * 0.1f, Quaternion.identity);
                 Destroy(target);
             }
